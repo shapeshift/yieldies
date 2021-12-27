5561ec37a4411d965967d8f1e17a9dc2c9cec8c2
0xean <0xean.eth@gmail.com>
2021-12-27 12:27:03 -0600

    adds some method stubs

--- src/contracts/StakingFox.sol
+++ src/contracts/StakingFox.sol
@@ -1,14 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
-contract StakingFox {
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract StakingFox is ERC20, ReentrancyGuard {
 
   address public immutable rewardToken;
 
   constructor(
     address _rewardToken
-  ) {
+  ) ERC20("Staked FOX", "sFOX") {
     rewardToken = _rewardToken;
+  }
+
+  function deposit(uint256 _amount) external nonReentrant() {
+
+  }
+
+
+  function requestWithdraw(uint256 _amount) external nonReentrant() {
+
+  }
+
+  function withdraw(uint256 _amount) external nonReentrant() {
+
+  }
+
+
+  function withdrawWithPenalty(uint256 _amount) external nonReentrant() {
+
+  }
+
+  function rewardStakers(uint256 _amount) external nonReentrant() {
+
   }
 
 }
