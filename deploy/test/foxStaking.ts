da2126b231737f5805e49b27090219021af8cf6d
0xean <0xean.eth@gmail.com>
2021-12-28 16:20:10 -0600

    fixes block number

--- deploy/test/foxStaking.ts
+++ deploy/test/foxStaking.ts
@@ -1,18 +1,20 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, ethers } = hre;
   const { deploy } = deployments;
 
   const { admin } = await getNamedAccounts();
-
   const sFox = await deployments.get("sFox");
   const fox = await deployments.get("Fox"); // mock fox token
 
   const epochLength = 100; 
   const firstEpochNumber = 1;
-  const firstEpochBlock = 1;
+  const currentBlock = await ethers.provider.getBlockNumber();
+  const firstEpochBlock = currentBlock + epochLength;
 
   await deploy("FoxStaking", {
     from: admin,
