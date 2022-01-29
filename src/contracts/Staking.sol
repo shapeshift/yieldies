4befa7c93a75d6ce2a14752ca916705e7c309afd
toshiSat <10103480+toshiSat@users.noreply.github.com>
2022-01-29 11:34:42 -0700

    better file org

--- src/contracts/Staking.sol
+++ src/contracts/Staking.sol
@@ -1,74 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.8.11;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "hardhat/console.sol";
 import "./Vesting.sol";
-
-interface IOwnable {
-    function manager() external view returns (address);
-
-    function renounceManagement() external;
-
-    function pushManagement(address newOwner_) external;
-
-    function pullManagement() external;
-}
-
-contract Ownable is IOwnable {
-    address internal _owner;
-    address internal _newOwner;
-
-    event OwnershipPushed(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-    event OwnershipPulled(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-
-    constructor() {
-        _owner = msg.sender;
-        emit OwnershipPushed(address(0), _owner);
-    }
-
-    function manager() public view override returns (address) {
-        return _owner;
-    }
-
-    modifier onlyManager() {
-        require(_owner == msg.sender, "Ownable: caller is not the owner");
-        _;
-    }
-
-    function renounceManagement() public virtual override onlyManager {
-        emit OwnershipPushed(_owner, address(0));
-        _owner = address(0);
-    }
-
-    function pushManagement(address newOwner_)
-        public
-        virtual
-        override
-        onlyManager
-    {
-        require(
-            newOwner_ != address(0),
-            "Ownable: new owner is the zero address"
-        );
-        emit OwnershipPushed(_owner, newOwner_);
-        _newOwner = newOwner_;
-    }
-
-    function pullManagement() public virtual override {
-        require(msg.sender == _newOwner, "Ownable: must be new owner to pull");
-        emit OwnershipPulled(_owner, _newOwner);
-        _owner = _newOwner;
-    }
-}
+import "./types/Ownable.sol";
 
 interface IRewardToken {
     function rebase(uint256 ohmProfit_, uint256 epoch_)
