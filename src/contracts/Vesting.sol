27d1ce986b297826cbd5e69054b255ca1b61233a
MrNerdHair <mrnerdhair@gmail.com>
2022-03-24 15:42:36 -0400

    Update Vesting.sol

--- src/contracts/Vesting.sol
+++ src/contracts/Vesting.sol
@@ -4,16 +4,16 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Vesting {
-    address public immutable STAKING_TOKEN;
+    address public immutable STAKING_CONTRACT;
     address public immutable REWARD_TOKEN;
 
-    constructor(address _stakingToken, address _rewardToken) {
+    constructor(address _stakingContract, address _rewardToken) {
         // addresses can't be 0x0
         require(
-            _stakingToken != address(0) && _rewardToken != address(0),
+            _stakingContract != address(0) && _rewardToken != address(0),
             "Invalid address"
         );
-        STAKING_TOKEN = _stakingToken;
+        STAKING_CONTRACT = _stakingContract;
         REWARD_TOKEN = _rewardToken;
     }
 
@@ -25,7 +25,7 @@ contract Vesting {
     function retrieve(address _staker, uint256 _amount) external {
         // must be called from staking contract
         require(
-            msg.sender == STAKING_TOKEN,
+            msg.sender == STAKING_CONTRACT,
             "Not called from staking contract"
         );
         IERC20(REWARD_TOKEN).transfer(_staker, _amount);
