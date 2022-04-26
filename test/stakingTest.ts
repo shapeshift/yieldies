974f0f3b5b70b9a98abd6704c896de354fe99745
toshiSat <10103480+toshiSat@users.noreply.github.com>
2022-04-26 08:16:28 -0600

    lint

--- test/stakingTest.ts
+++ test/stakingTest.ts
@@ -38,8 +38,9 @@ describe("Staking", function () {
     const epoch = await staking.epoch();
     const cycleDuration = await tokeManager.getCycleDuration();
     const cyceleStart = await tokeManager.getCurrentCycle();
-    const tokeEndTime = BigNumber.from(cyceleStart).add(cycleDuration)
-    const duration = tokeEndTime < epoch.endTime ? epoch.duration : cycleDuration
+    const tokeEndTime = BigNumber.from(cyceleStart).add(cycleDuration);
+    const duration =
+      tokeEndTime < epoch.endTime ? epoch.duration : cycleDuration;
     await network.provider.send("evm_increaseTime", [Number(duration) + 10]);
     await network.provider.send("hardhat_mine");
   }
